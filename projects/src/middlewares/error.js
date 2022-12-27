b605bb3758fcf4a3dfee850de318bc37ccd02cb1
nwnp <jjw_0418@naver.com>
2022-12-27 22:02:48 +0900

    파일 삭제

--- projects/src/middlewares/error.js
+++
@@ -1,4 +0,0 @@
-export const error = (err, req, res, next) => {
-  console.log(err);
-  res.send(500, "Something broke. What did you do?");
-};
--- projects/src/middlewares/errorHandlers.js
+++
@@ -1,4 +0,0 @@
-export const notFound = (req, res, next) => {
-  res.send(404, "You seem lost. You must have taken a wrong turn back there.");
-  next();
-};
